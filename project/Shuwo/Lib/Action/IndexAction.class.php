5a8e091524dffbc8fa67649c94c7f8a928e27cda
melodydx <daixi66@163.com>
2014-12-19 23:41:12 +0800

    加入配置文件
    加入配置文件

--- project/Shuwo/Lib/Action/IndexAction.class.php
+++ project/Shuwo/Lib/Action/IndexAction.class.php
@@ -2,6 +2,10 @@
 // 本类由系统自动生成，仅供测试用途
 class IndexAction extends Action {
     public function index(){
-    $this->redirect('Home/index');
+    
+    	$Data = M('product'); // 实例化Data数据模型
+    	$this->data = $Data->select();
+    	$this->display();
+    	
     }
 }
