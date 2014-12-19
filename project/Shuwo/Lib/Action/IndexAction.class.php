d62b1199601ca4ba00b3d80c234cdc6e5934baa2
melodydx <daixi66@163.com>
2014-12-19 18:49:47 +0800

    add project
    add project

---
+++ project/Shuwo/Lib/Action/IndexAction.class.php
@@ -0,0 +1,13 @@
+<?php
+// 本类由系统自动生成，仅供测试用途
+class IndexAction extends Action {
+    public function index(){
+<<<<<<< HEAD:project/Shuwo/Lib/Action/IndexAction.class.php
+    	$a = 1;
+    $this->display();
+    }}
+=======
+    $this->redirect('Home/index');
+    }
+}
+>>>>>>> origin/master:solution/Shuwo/Lib/Action/IndexAction.class.php
