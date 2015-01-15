2fa6bf8014b0bfc6142179e5b8b2b853f2d8bab5
Alex Dai <daixi66@163.com>
2015-01-15 14:37:17 +0800

    修改数据库脚本文件
    修改数据库脚本文件

--- 数据库脚本/shuwo.sql
+++ 数据库脚本/shuwo.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50617
 File Encoding         : 65001
 
-Date: 2015-01-15 11:19:43
+Date: 2015-01-15 14:36:19
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,13 +20,13 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `order`;
 CREATE TABLE `order` (
-  `OrderID` varchar(32) CHARACTER SET utf8 NOT NULL,
+  `OrderID` varchar(32) NOT NULL,
   `OrderStatus` int(10) unsigned zerofill NOT NULL,
   `UserID` int(11) NOT NULL,
   `PaymentStatus` int(11) unsigned zerofill NOT NULL,
   `CreatedTime` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`OrderID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for shippingaddress
@@ -35,36 +35,36 @@ DROP TABLE IF EXISTS `shippingaddress`;
 CREATE TABLE `shippingaddress` (
   `SAID` int(11) NOT NULL AUTO_INCREMENT,
   `UserID` int(11) NOT NULL,
-  `UserName` varchar(8) CHARACTER SET utf8 DEFAULT NULL,
-  `Province` varchar(32) CHARACTER SET utf8 DEFAULT NULL,
-  `City` varchar(32) CHARACTER SET utf8 DEFAULT NULL,
-  `Distinct` varchar(32) CHARACTER SET utf8 DEFAULT NULL,
-  `Address` varchar(64) CHARACTER SET utf8 DEFAULT NULL,
-  `Mobile` varchar(16) CHARACTER SET utf8 DEFAULT NULL,
+  `UserName` varchar(8) DEFAULT NULL,
+  `Province` varchar(32) DEFAULT NULL,
+  `City` varchar(32) DEFAULT NULL,
+  `Distinct` varchar(32) DEFAULT NULL,
+  `Address` varchar(64) DEFAULT NULL,
+  `Mobile` varchar(16) DEFAULT NULL,
   `IsDefault` tinyint(3) unsigned DEFAULT '1',
   PRIMARY KEY (`SAID`),
   KEY `shippingaddress_ibfk_userid` (`UserID`),
   CONSTRAINT `shippingaddress_ibfk_userid` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for shop
 -- ----------------------------
 DROP TABLE IF EXISTS `shop`;
 CREATE TABLE `shop` (
-  `ShopID` varchar(16) CHARACTER SET utf8 NOT NULL,
-  `ShopAddress` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
-  `ShopName` varchar(32) CHARACTER SET utf8 DEFAULT NULL,
-  `ContactName` varchar(8) CHARACTER SET utf8 DEFAULT NULL,
-  `ContactPhone` varchar(16) CHARACTER SET utf8 DEFAULT NULL,
+  `ShopID` varchar(16) NOT NULL,
+  `ShopAddress` varchar(255) DEFAULT NULL,
+  `ShopName` varchar(32) DEFAULT NULL,
+  `ContactName` varchar(8) DEFAULT NULL,
+  `ContactPhone` varchar(16) DEFAULT NULL,
   `Latitude` double NOT NULL,
-  `Attitude` double NOT NULL,
+  `Longitude` double NOT NULL,
   `GeoHash` varchar(32) NOT NULL,
-  `City` varchar(16) CHARACTER SET utf8 DEFAULT NULL,
-  `Distinct` varchar(16) CHARACTER SET utf8 DEFAULT NULL,
-  `Province` varchar(16) CHARACTER SET utf8 DEFAULT NULL,
+  `City` varchar(16) DEFAULT NULL,
+  `Distinct` varchar(16) DEFAULT NULL,
+  `Province` varchar(16) DEFAULT NULL,
   PRIMARY KEY (`ShopID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for user
@@ -72,14 +72,14 @@ CREATE TABLE `shop` (
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `UserID` int(11) NOT NULL AUTO_INCREMENT,
-  `UnionID` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `Name` varchar(32) CHARACTER SET utf8 DEFAULT NULL,
-  `Password` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
-  `Mobile` varchar(16) CHARACTER SET utf8 DEFAULT NULL,
+  `UnionID` varchar(255) NOT NULL,
+  `Name` varchar(32) DEFAULT NULL,
+  `Password` varchar(255) DEFAULT NULL,
+  `Mobile` varchar(16) DEFAULT NULL,
   `Sex` int(11) unsigned zerofill DEFAULT '00000000000',
-  `City` varchar(8) CHARACTER SET utf8 DEFAULT NULL,
-  `Province` varchar(8) CHARACTER SET utf8 DEFAULT NULL,
-  `Country` varchar(8) CHARACTER SET utf8 DEFAULT NULL,
+  `City` varchar(8) DEFAULT NULL,
+  `Province` varchar(8) DEFAULT NULL,
+  `Country` varchar(8) DEFAULT NULL,
   `CreatedTime` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`UserID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
