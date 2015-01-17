46d4e30875fe1e0f1cd7f06c151c600120dea1f4
melodydx <daixi66@163.com>
2015-01-17 11:47:24 +0800

    修改首页
    修改首页

--- project/Shuwo/Lib/Action/IndexAction.class.php
+++ project/Shuwo/Lib/Action/IndexAction.class.php
@@ -1,20 +1,40 @@
 <?php
 require_once 'DAL/ShopDAL.php';
+require_once 'DAL/UserDAL.php';
+require_once 'Utilities/UserConst.php';
+require_once 'Utilities/WeixinAuthorizer.php';
+require_once 'BAL/GeoHash.php';
 class IndexAction extends Action {
     public function index(){
-    	/*$userdal= new UserDAL();
-    	$user = M('user');
-    	if (WeixinAuthorizer::AuthorizUser())
-    	{
-    		$user = WeixinAuthorizer::AuthorizUser();
-    		$userid =  $userdal->InsertUser($user);
-     	   
-    	} $_SESSION[UserConst::USERID]=$userid;
-    	$productdal = new ProductDAL();
-    	$this->data = $productdal->GetAllProduct();*/
-    	
-    	//$shopDAL = new ShopDAL();
-    	//$data =$shopDAL->GetShopByID("S0001");
+           $userDal = new UserDAL();
+           if(WeixinAuthorizer::AuthorizUser())
+           {
+           	  $user = WeixinAuthorizer::AuthorizUser();
+           	  if($user)
+           	  {
+           	    $userid = $userDal->InsertUser($user);
+           	  	$_SESSION[UserConst::USERID]  =$userid;
+           	  }
+           }
+           
+           // 查询店铺
+           $latitude=$_POST['latitude_hidden'];
+           $longitude=$_POST['longitude_hidden'];
+           $geoHash = new Geohash();
+           $geoHashCode =$geoHash->encode($latitude,$longitude);
+           $length = 5;
+           $likeGeoHash = substr($geoHashCode, 0,$length);
+           $sql = 'GeoHash like'.$likeGeoHash.'%';
+           $shop = M("shop");
+           $data =  $shop->where($sql)->select();
+           if(count($data))
+           {
+           	foreach ($data as $item)
+           	{
+           		
+           	}
+           }
+           
     	$this->display();
     }
 }
--- project/Shuwo/Temp/Cache/089285936dd8b507a637811b17b7be45.php
+++ project/Shuwo/Temp/Cache/089285936dd8b507a637811b17b7be45.php
@@ -1,10 +1,39 @@
-<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
-<html>
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>首页</title>
+<script src="__JS__/jquery.js"></script>
+<script type="text/javascript" src="__JS__/area.js"></script>
+<script type="text/javascript" src="__JS__/location.js"></script>
+<script src="__JS__/select2.js"></script>
+<script src="__JS__/select2_locale_zh-CN.js"></script>
+<link href="__CSS__/common.css" rel="stylesheet"/>
+<link href="__CSS__/select2.css" rel="stylesheet"/>
+<script type="text/javascript" src="http://api.map.baidu.com/api?v=1.5&ak=lNKQpXsBLNjd9N4zxpiok9yz"></script>
+
 </head>
 <body>
- <br>test</br>
+<form action="<?php echo U('Index/index/');?>" method="post">
+<div class="main">
+<input type="hidden" id="longitude_hidden" name="longitude_hidden" value=""/>
+<input type="hidden" id="latitude_hidden" name="latitude_hidden" value=""/>
+  <center>
+  <select id="loc_province" name="Province" style="width:120px;">
+  </select>
+  <select id="loc_city" name="City" style="width:120px; margin-left: 10px">
+  </select>
+  <select id="loc_town" name="Distinct" style="width:120px;margin-left: 10px">
+  </select>
+           详细地址：<input type="text" id="ShopAddress" name="ShopAddress"/>
+           <input type="submit" style ="width:32px" value="确定" />
+           <br></br>
+  </center>
+  
+<div id="container" style="border:1px solid red;weight:900px;height:400px;margin:0px auto;overflow:hidden;">
+</div>
+	<script type="text/javascript" src="__JS__/baidumap.js"/>
+</div>
+</form>
 </body>
 </html>
